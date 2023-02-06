0a385077a741d8da612e45f97f61ae4a5bd3105e
aida niese <alniese@iu.edu>
2023-02-06 17:11:37 -0500

    Niese Lightning Talk

---
+++ Niese Lightning Talk.py
@@ -0,0 +1,21 @@
+#Niese Lightning Talk 
+
+#cooking conversions 
+
+def oz_to_cups(ounces):
+    cups = ounces*0.125 
+    print ("there are", cups, "cups in", ounces, "ounces")
+    
+oz_to_cups(14)
+
+def grams_to_cups(grams):
+    cups = float(grams/220)
+    print ("there are",cups,"cups in", grams,"grams")
+
+grams_to_cups(483)
+
+def oz_to_grams(ounces):
+    grams = ounces*28.3495
+    print ("there are", grams,"grams in", ounces ,"ounces")
+    
+oz_to_grams(83)
